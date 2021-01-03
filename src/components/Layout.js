9c4e0159517d759a3c61c480a208b7f14ced6749
Richard <richardmenning@googlemail.com>
2021-01-03 15:36:41 +0100

    prepare layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,12 +6,12 @@ const Layout = ({ children }) => {
     <>
       <SEO />
       <div className="flex flex-col min-h-screen bg-gray-200">
-        <header className="p-4 bg-teal-500 text-white font-semibold">
-          <h1 className="max-w-4xl mx-auto">gatsby-starter-tailwind-css</h1>
+        <header className="p-4 bg-green-700 text-white font-semibold">
+          <h1 className="max-w-4xl mx-auto">The Shoppies</h1>
         </header>
         {children}
         <footer className="py-2 text-center text-gray-600 text-xs">
-          &copy; Melanie Nolan 2020
+          Made by <a href="https://github.com/RichardMEN11">Richard Menning</a>
         </footer>
       </div>
     </>
