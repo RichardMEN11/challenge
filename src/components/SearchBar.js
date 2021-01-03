514a194754912c6d81dda10ceffd39b92b32f6e2
Richard <richardmenning@googlemail.com>
2021-01-03 16:59:06 +0100

    searchbar as own component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+const SearchBar = ({ handleSubmit, setSearchTerm, searchTerm }) => {
+  return (
+    <section className="w-full h-1/3 flex justify-center">
+      <form
+        className="bg-gray-200 w-8/12 my-10 p-10 rounded"
+        onSubmit={e => {
+          e.preventDefault()
+          handleSubmit()
+        }}
+      >
+        <label
+          htmlFor="title"
+          className="block mb-2 text-lg text-gray-700 uppercase font-bold"
+        >
+          Movie Title <span role="img">🎬</span>
+        </label>
+        <input
+          type="text"
+          name="title"
+          onChange={e => {
+            setSearchTerm(e.target.value)
+          }}
+          value={searchTerm}
+          className="py-2 px-3 w-10/12 rounded-l text-grey-darkest"
+        />
+        <input
+          type="submit"
+          value="Search 🔎"
+          className="bg-gray-600 py-2 px-6 text-white rounded-r font-bold cursor-pointer hover:bg-gray-500"
+        />
+        <small className="text-gray-500 text-center block mt-1">
+          Please add a title you would like to search for.
+        </small>
+      </form>
+    </section>
+  )
+}
+
+export default SearchBar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import Layout from "../components/Layout"
 import axios from "axios"
+import SearchBar from "../components/SearchBar"
 
 const IndexPage = () => {
   const [searchTerm, setSearchTerm] = useState("")
@@ -19,29 +20,11 @@ const IndexPage = () => {
   }
   return (
     <Layout>
-      <section className="w-full h-1/3 flex justify-center">
-        <form
-          className="bg-gray-200 w-8/12 my-10 p-10 rounded"
-          onSubmit={e => {
-            e.preventDefault()
-            handleSubmit()
-          }}
-        >
-          <label htmlFor="title" className="block mb-2">
-            Movie Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            onChange={e => {
-              setSearchTerm(e.target.value)
-            }}
-            value={searchTerm}
-            className="py-2 px-1 w-10/12 rounded"
-          />
-          <input type="submit" value="Search" className="ml-3" />
-        </form>
-      </section>
+      <SearchBar
+        handleSubmit={handleSubmit}
+        setSearchTerm={setSearchTerm}
+        searchTerm={searchTerm}
+      />
       <section className="mx-auto w-8/12 h-1/3 flex justify-between">
         <div className="bg-gray-200 w-5/12">
           <h3>Results for {searchTerm}</h3>
