3474c754432dd1359c60452fa97612c5baed07f8
Richard <richardmenning@googlemail.com>
2021-01-08 07:21:10 +0100

    add loading indicator

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -5,35 +5,75 @@ import ResultItem from "./ResultItem"
 const ResultList = styled.ul`
   max-height: 400px;
   overflow-y: scroll;
+  &::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+    background-color: #f5f5f5;
+    border-radius: 4px;
+  }
+  &::-webkit-scrollbar {
+    width: 10px;
+    background-color: #f5f5f5;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background-color: #2f855a;
+  }
 `
 
-const SearchResults = ({ searchResults, searchTerm, handleNomination }) => {
+const Loader = styled.div`
+  border: 2px solid #fff; 
+  border-top: 2px solid #2f855a; 
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: spin 1.5s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+`
+
+const SearchResults = ({
+  searchResults,
+  searchTerm,
+  handleNomination,
+  loading,
+}) => {
   return (
-    <div className="bg-gray-200 w-6/12 md:w-12/12 py-5 pl-3 rounded">
+    <div className="bg-gray-200 w-full md:w-6/12 py-5 pl-3 rounded">
       <h3 className="text-lg uppercase text-gray-700 font-bold mb-3">
         Results for{" "}
         <span className="underline font-extrabold">{searchTerm}</span>
       </h3>
-
-      {typeof searchResults !== "undefined" && searchResults.length > 0 ? (
-        <>
-          <ResultList>
-            {searchResults.map(result => (
-              <ResultItem
-                key={result.imdbID}
-                result={result}
-                handleNomination={handleNomination}
-              ></ResultItem>
-            ))}
-          </ResultList>
-          <div className="text-center my-2">
-            <small className="text-gray-500">
-              Series = 🎞{"  "}|{"  "}Movie = 📽
-            </small>
-          </div>
-        </>
+      {loading ? (
+        <div className="flex justify-center items-center w-100">
+          <Loader />
+        </div>
       ) : (
-        <p>Nothing to show here…</p>
+        <>
+          {typeof searchResults !== "undefined" && searchResults.length > 0 ? (
+            <>
+              <ResultList>
+                {searchResults.map(result => (
+                  <ResultItem
+                    key={result.imdbID}
+                    result={result}
+                    handleNomination={handleNomination}
+                  ></ResultItem>
+                ))}
+              </ResultList>
+              <div className="text-center my-2">
+                <small className="text-gray-500">
+                  Series = 🎞{"  "}|{"  "}Movie = 📽
+                </small>
+              </div>
+            </>
+          ) : (
+            <p>Nothing to show here…</p>
+          )}
+        </>
       )}
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../components/Layout"
 import axios from "axios"
 import SearchBar from "../components/SearchBar"
@@ -9,13 +9,16 @@ const IndexPage = () => {
   const [searchTerm, setSearchTerm] = useState("")
   const [searchResults, setSearchResults] = useState([])
   const [nominations, setNominations] = useState([])
+  const [loading, setLoading] = useState(false)
 
   const handleSubmit = async e => {
     try {
+      setLoading(true)
       const data = await axios.get(
         `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${searchTerm}`
       )
       setSearchResults(data.data.Search)
+      setLoading(false)
     } catch (error) {
       console.log(error)
     }
@@ -40,11 +43,12 @@ const IndexPage = () => {
         setSearchTerm={setSearchTerm}
         searchTerm={searchTerm}
       />
-      <section className="mx-auto w-8/12 h-1/3 flex justify-between">
+      <section className="mx-auto w-8/12 h-1/3 flex justify-between flex-col sm:flex-row">
         <SearchResults
           searchResults={searchResults}
           searchTerm={searchTerm}
           handleNomination={handleNomination}
+          loading={loading}
         />
         <Nominations
           nominations={nominations}
