8e3c5683d1c67ccef3fcc5e5356d9e35479d3838
Richard <richardmenning@googlemail.com>
2021-01-08 08:51:45 +0100

    change title and author

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,23 +9,12 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter Tailwind CSS",
-    author: "Melanie Nolan",
+    title: "The Shoppies",
+    author: "Richard Menning",
     description: "A Gatsby starter to set you up with Tailwind CSS",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        name: "Gatsby Starter Tailwind CSS",
-        short_name: "Gatsby Starter Tailwind CSS",
-        start_url: "/",
-        background_color: theme.colors.white,
-        theme_color: theme.colors.teal[500],
-        icon: "static/icon.svg",
-      },
-    },
     {
       resolve: "gatsby-plugin-postcss",
       options: {
